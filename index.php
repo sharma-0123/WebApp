159d30bb5b40a2da417ac73eeba2540dadd9ec74
sharma-0123 <56450252+sharma-0123@users.noreply.github.com>
2022-02-13 05:07:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
+$servername = "mpsscqueueflexdb.mysql.database.azure.com";
+$database = "mpsscqueueflexdb";
+$username = "flexdbadmin";
+$password = "DBAdmin!@#";
 
-echo "Testing";
+// Create connection
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+echo “Connected successfully”;
+
+mysqli_close($conn);
 
 ?>
-
